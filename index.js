f69c850c757938e3579cd35aabc4a746c4692189
Joshua Haughton <itsjoshuahaughton@gmail.com>
2021-10-30 04:16:53 +0000

    recurses through any amount of nested arrays to return the summed value of all integer elements in all nested arrays

--- index.js
+++ index.js
@@ -1,5 +1,49 @@
 function sumItems(array) {
-  // Sum all the numbers in the array
-}
+  if (array.length === 0) {
+    return null;
+  }
+  let out = [];
+  console.log(array)
+  
+  function helper (array) {
+    for (let i = 0; i < array.length; i++) {
+      let element = array[i]
+
+      if (Array.isArray(element)) {
+        helper(element);
+      } else {
+        out.push(element)
+        helper(element)
+      }
+
+    }
+    
+     
+  }
+  helper(array);
+  console.log(out)
+  return out.reduce((a,b) => a + b)
+  }
+
+
+  // if (array.length > 0) {
+  //   console.log(array);
+  //   return (array[(array.length)-1] + sumItems(array.pop()));
+  // }
+  
+  // array.forEach(item => {
+  //   if (Array.isArray(item)) {
+  //     // Print out all it's items individually
+  //     sumItems(item);
+  //   } else {
+  //     return (item + sumItems(array + 1));
+  //   }
+  
+  // })
+
+
+
+
+// sumItems([1, 2, 3, 4, 5], )
 
 module.exports = sumItems;
